aa33d070fcb41298f1135f830d22561f5cece7da
Manoel Ribeiro <manoelribeiro20181@gmail.com>
2025-03-20 22:22:14 -0300

    Creating base entity

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Entities/Sale.cs
@@ -0,0 +1,14 @@
+﻿namespace Ambev.DeveloperEvaluation.Domain.Entities
+{
+    public class Sale
+    {
+        public Guid Id { get; set; }
+        public string Number { get; set; }
+        public Guid UserId { get; set; }
+        public Guid BranchId { get; set; }
+        public decimal TotalSaleAmount { get; set; }
+        public DateTime Date { get; set; } = DateTime.UtcNow;
+        public List<SaleITem> Products { get; set; } = [];
+        public bool IsCanceled { get; set; }
+    }
+}
